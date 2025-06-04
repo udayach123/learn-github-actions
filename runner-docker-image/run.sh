a828c66f3e7c72a9d070ff6f0e23604857d6a196
Raghu K <raghuk.vit@gmail.com>
2025-06-04 06:54:22 +0530

    RoboShop Project

--- runner-docker-image/run.sh
+++ runner-docker-image/run.sh
@@ -1,5 +1,5 @@
 if [ -z "${ORG}" ]; then echo Input ORG is missing ; exit 1 ; fi
 if [ -z "${TOKEN}" ]; then echo Input TOKEN is missing ; exit 1 ; fi
-if [ -z "${TOKEN}" ]; then echo Input TOKEN is missing ; exit 1 ; fi
-./config.sh --unattended --url ${ORG} --token ${TOKEN} --name ${TOKEN} --runnergroup Default --work _work
+if [ -z "${NAME}" ]; then echo Input NAME is missing ; exit 1 ; fi
+./config.sh --unattended --url ${ORG} --token ${TOKEN} --name ${NAME} --runnergroup Default --work _work
 ./run.sh
